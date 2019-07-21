4cf9e877f2316093c10fd5e44211160ce66d8a7b
Web <web@it.step>
2019-07-21 18:00:43 +0300

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,30 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
+<header class="header">
 
+</header>
+<footer class="footer">
+    <div class="home-slider">
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+    </div>
+</footer>
+<link rel="stylesheet" type="text/css" href="{{ asset('/libs/slick/slick.css') }}"/>
+<link rel="stylesheet" type="text/css" href="{{ asset('/libs/slick/slick-theme.css) }}"/>
+<script type="text/javascript" src="{{ asset('/libs/slick/slick.min.js) }}"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.home-slider').slick({
+            arrows: true,
+            dots: true
+        });
+    });
+</script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.sass', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css');
+mix.version();
+mix.copy('node_modules/slick-carousel/slick', 'public/assets/slick');
 
